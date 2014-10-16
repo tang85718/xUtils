b1ef8ef73531b225b32c2fb62f4aac5034f159cc
tang85718 <daulgas.tang@didian.me>
2014-10-16 10:45:29 +0800

    添加groupBy，修复orderBy

--- library/src/com/lidroid/xutils/db/sqlite/Selector.java
+++ library/src/com/lidroid/xutils/db/sqlite/Selector.java
@@ -15,156 +15,178 @@
 
 package com.lidroid.xutils.db.sqlite;
 
-import com.lidroid.xutils.db.table.TableUtils;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import com.lidroid.xutils.db.table.TableUtils;
+
 /**
- * Author: wyouflf
- * Date: 13-8-9
- * Time: 下午10:19
+ * Author: wyouflf Date: 13-8-9 Time: 下午10:19
+ * 
+ * @author tangxuyao 2014-10-13 修改groupBy
  */
 public class Selector {
 
-    protected Class<?> entityType;
-    protected String tableName;
+	protected Class<?> entityType;
+	protected String tableName;
 
-    protected WhereBuilder whereBuilder;
-    protected List<OrderBy> orderByList;
-    protected int limit = 0;
-    protected int offset = 0;
+	protected WhereBuilder whereBuilder;
+	protected List<OrderBy> orderByList;
+	protected List<String> groupByList;
+	protected int limit = 0;
+	protected int offset = 0;
 
-    private Selector(Class<?> entityType) {
-        this.entityType = entityType;
-        this.tableName = TableUtils.getTableName(entityType);
-    }
+	private Selector(Class<?> entityType) {
+		this.entityType = entityType;
+		this.tableName = TableUtils.getTableName(entityType);
+	}
 
-    public static Selector from(Class<?> entityType) {
-        return new Selector(entityType);
-    }
+	public static Selector from(Class<?> entityType) {
+		return new Selector(entityType);
+	}
 
-    public Selector where(WhereBuilder whereBuilder) {
-        this.whereBuilder = whereBuilder;
-        return this;
-    }
+	public Selector where(WhereBuilder whereBuilder) {
+		this.whereBuilder = whereBuilder;
+		return this;
+	}
 
-    public Selector where(String columnName, String op, Object value) {
-        this.whereBuilder = WhereBuilder.b(columnName, op, value);
-        return this;
-    }
+	public Selector where(String columnName, String op, Object value) {
+		this.whereBuilder = WhereBuilder.b(columnName, op, value);
+		return this;
+	}
 
-    public Selector and(String columnName, String op, Object value) {
-        this.whereBuilder.and(columnName, op, value);
-        return this;
-    }
+	public Selector and(String columnName, String op, Object value) {
+		this.whereBuilder.and(columnName, op, value);
+		return this;
+	}
 
-    public Selector and(WhereBuilder where) {
-        this.whereBuilder.expr("AND (" + where.toString() + ")");
-        return this;
-    }
+	public Selector and(WhereBuilder where) {
+		this.whereBuilder.expr("AND (" + where.toString() + ")");
+		return this;
+	}
 
-    public Selector or(String columnName, String op, Object value) {
-        this.whereBuilder.or(columnName, op, value);
-        return this;
-    }
+	public Selector or(String columnName, String op, Object value) {
+		this.whereBuilder.or(columnName, op, value);
+		return this;
+	}
 
-    public Selector or(WhereBuilder where) {
-        this.whereBuilder.expr("OR (" + where.toString() + ")");
-        return this;
-    }
+	public Selector or(WhereBuilder where) {
+		this.whereBuilder.expr("OR (" + where.toString() + ")");
+		return this;
+	}
 
-    public Selector expr(String expr) {
-        if (this.whereBuilder == null) {
-            this.whereBuilder = WhereBuilder.b();
-        }
-        this.whereBuilder.expr(expr);
-        return this;
-    }
+	public Selector expr(String expr) {
+		if (this.whereBuilder == null) {
+			this.whereBuilder = WhereBuilder.b();
+		}
+		this.whereBuilder.expr(expr);
+		return this;
+	}
 
-    public Selector expr(String columnName, String op, Object value) {
-        if (this.whereBuilder == null) {
-            this.whereBuilder = WhereBuilder.b();
-        }
-        this.whereBuilder.expr(columnName, op, value);
-        return this;
-    }
+	public Selector expr(String columnName, String op, Object value) {
+		if (this.whereBuilder == null) {
+			this.whereBuilder = WhereBuilder.b();
+		}
+		this.whereBuilder.expr(columnName, op, value);
+		return this;
+	}
 
-    public DbModelSelector groupBy(String columnName) {
-        return new DbModelSelector(this, columnName);
-    }
+	// public DbModelSelector groupBy(String columnName) {
+	// return new DbModelSelector(this, columnName);
+	// }
 
-    public DbModelSelector select(String... columnExpressions) {
-        return new DbModelSelector(this, columnExpressions);
-    }
+	public Selector groupBy(String columnName) {
 
-    public Selector orderBy(String columnName) {
-        if (orderByList == null) {
-            orderByList = new ArrayList<OrderBy>(2);
-        }
-        orderByList.add(new OrderBy(columnName));
-        return this;
-    }
+		if (groupByList == null) {
+			groupByList = new ArrayList<String>();
+		}
 
-    public Selector orderBy(String columnName, boolean desc) {
-        if (orderByList == null) {
-            orderByList = new ArrayList<OrderBy>(2);
-        }
-        orderByList.add(new OrderBy(columnName, desc));
-        return this;
-    }
+		groupByList.add(columnName);
+		return this;
+	}
 
-    public Selector limit(int limit) {
-        this.limit = limit;
-        return this;
-    }
+	public DbModelSelector select(String... columnExpressions) {
+		return new DbModelSelector(this, columnExpressions);
+	}
 
-    public Selector offset(int offset) {
-        this.offset = offset;
-        return this;
-    }
+	public Selector orderBy(String columnName) {
+		if (orderByList == null) {
+			orderByList = new ArrayList<OrderBy>(2);
+		}
+		orderByList.add(new OrderBy(columnName));
+		return this;
+	}
 
-    @Override
-    public String toString() {
-        StringBuilder result = new StringBuilder();
-        result.append("SELECT ");
-        result.append("*");
-        result.append(" FROM ").append(tableName);
-        if (whereBuilder != null && whereBuilder.getWhereItemSize() > 0) {
-            result.append(" WHERE ").append(whereBuilder.toString());
-        }
-        if (orderByList != null) {
-            for (int i = 0; i < orderByList.size(); i++) {
-                result.append(" ORDER BY ").append(orderByList.get(i).toString());
-            }
-        }
-        if (limit > 0) {
-            result.append(" LIMIT ").append(limit);
-            result.append(" OFFSET ").append(offset);
-        }
-        return result.toString();
-    }
+	public Selector orderBy(String columnName, boolean desc) {
+		if (orderByList == null) {
+			orderByList = new ArrayList<OrderBy>(2);
+		}
+		orderByList.add(new OrderBy(columnName, desc));
+		return this;
+	}
 
-    public Class<?> getEntityType() {
-        return entityType;
-    }
+	public Selector limit(int limit) {
+		this.limit = limit;
+		return this;
+	}
 
-    protected class OrderBy {
-        private String columnName;
-        private boolean desc;
+	public Selector offset(int offset) {
+		this.offset = offset;
+		return this;
+	}
 
-        public OrderBy(String columnName) {
-            this.columnName = columnName;
-        }
+	@Override
+	public String toString() {
+		StringBuilder result = new StringBuilder();
+		result.append("SELECT ");
+		result.append("*");
+		result.append(" FROM ").append(tableName);
+		if (whereBuilder != null && whereBuilder.getWhereItemSize() > 0) {
+			result.append(" WHERE ").append(whereBuilder.toString());
+		}
 
-        public OrderBy(String columnName, boolean desc) {
-            this.columnName = columnName;
-            this.desc = desc;
-        }
+		if (groupByList != null) {
+			result.append(" GROUP BY ");
+			for (int i = 0; i < groupByList.size(); i++) {
+				result.append(groupByList.get(i)).append(",");
+			}
+			result.deleteCharAt(result.length() - 1);
+		}
 
-        @Override
-        public String toString() {
-            return columnName + (desc ? " DESC" : " ASC");
-        }
-    }
+		if (orderByList != null) {
+			result.append(" ORDER BY ");
+			for (int i = 0; i < orderByList.size(); i++) {
+				result.append(orderByList.get(i)).append(",");
+			}
+			result.deleteCharAt(result.length() - 1);
+		}
+		if (limit > 0) {
+			result.append(" LIMIT ").append(limit);
+			result.append(" OFFSET ").append(offset);
+		}
+		return result.toString();
+	}
+
+	public Class<?> getEntityType() {
+		return entityType;
+	}
+
+	protected class OrderBy {
+		private String columnName;
+		private boolean desc;
+
+		public OrderBy(String columnName) {
+			this.columnName = columnName;
+		}
+
+		public OrderBy(String columnName, boolean desc) {
+			this.columnName = columnName;
+			this.desc = desc;
+		}
+
+		@Override
+		public String toString() {
+			return columnName + (desc ? " DESC" : " ASC");
+		}
+	}
 }
